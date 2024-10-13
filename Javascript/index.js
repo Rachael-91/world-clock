d5cc273ed54608a63d50cbf4922af4c7edd4192c
Rachael Alexander <rachael@inox-world.com.au>
2024-10-13 17:01:11 +1100

    Minor fixes

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,55 +1,65 @@
+let citiesSelectElement = document.querySelector("#city");
+let timeUpdateInterval;
+
 function updateTime() {
+    // Los Angeles
+    let losAngelesElement = document.querySelector("#los-angeles");
+    if (losAngelesElement) {
+        let losAngelesDateElement = losAngelesElement.querySelector(".date");
+        let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+        let losAngelesTime = moment().tz("America/Los_Angeles");
 
-// Los Angeles
-let losAngelesElement = document.querySelector("#los-angeles");
-if (losAngelesElement) {
-let losAngelesDateElement = losAngelesElement.querySelector(".date");
-let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-let losAngelesTime = moment().tz("America/Los_Angeles");
+        losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+        losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss [<small>]A[</small>]");
+    }
 
-losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss [<small>]A[</small>]");
+    // Melbourne
+    let melbourneElement = document.querySelector("#melbourne");
+    if (melbourneElement) {
+        let melbourneDateElement = melbourneElement.querySelector(".date");
+        let melbourneTimeElement = melbourneElement.querySelector(".time");
+        let melbourneTime = moment().tz("Australia/Melbourne");
+
+        melbourneDateElement.innerHTML = melbourneTime.format("MMMM Do YYYY");
+        melbourneTimeElement.innerHTML = melbourneTime.format("h:mm:ss [<small>]A[</small>]");
+    }
 }
-
-// Melbourne
-let melbourneElement = document.querySelector("#melbourne");
-if (melbourneElement) {
-let melbourneDateElement = melbourneElement.querySelector(".date");
-let melbourneTimeElement = melbourneElement.querySelector(".time");
-let melbourneTime = moment().tz("Australia/Melbourne");
-
-melbourneDateElement.innerHTML = melbourneTime.format("MMMM Do YYYY");
-melbourneTimeElement.innerHTML = melbourneTime.format("h:mm:ss [<small>]A[</small>]");
-}
-};
-
-
 
 function updateCity(event) {
-    let cityTimeZone = event.target.value;
-    if (cityTimeZone === "current")
-    {cityTimeZone = moment.tz.guess()
+    // Clear existing interval if it exists
+    if (timeUpdateInterval) {
+        clearInterval(timeUpdateInterval);
+    }
 
+    let cityTimeZone = event.target.value;
+    if (cityTimeZone === "current") {
+        cityTimeZone = moment.tz.guess();
     }
     let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector("#cities");
 
     citiesElement.innerHTML = `
-    <div class="city">
-        <div>
-            <h2>${cityName}</h2>
-            <div class="date"> ${cityTime.format("MMMM Do YYYY")}</div>
-        </div>
+        <div class="city">
+            <div>
+                <h2>${cityName}</h2>
+                <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+            </div>
             <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
-    </div>
-    <a href="/">All Cities</a>
+        </div>
+        <a href="/">All Cities</a>
     `;
 
+    // Set a new interval to update the city time every second
+    timeUpdateInterval = setInterval(() => {
+        cityTime = moment().tz(cityTimeZone);
+        citiesElement.querySelector(".date").innerHTML = cityTime.format("MMMM Do YYYY");
+        citiesElement.querySelector(".time").innerHTML = `${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small>`;
+    }, 1000);
 }
-let citiesSelectElement = document.querySelector("#city")
+
 citiesSelectElement.addEventListener("change", updateCity);
 
-
+// Initial call to updateTime for other cities
 updateTime();
 setInterval(updateTime, 1000);
